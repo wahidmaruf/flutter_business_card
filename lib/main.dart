e267080bed3e97c55fc849eacf7b887b5507a0ed
Wahid Maruf <wahid.maruf@ibm.com>
2022-06-04 20:40:42 -0300

    Change status bar color to light

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,68 +12,70 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.deepPurple,
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('images/karl.jpg'),
-              ),
-              const Text('Karl Urban',
-                  style: TextStyle(
-                      fontFamily: 'Pacifico',
-                      fontSize: 40.0,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold)),
-              SizedBox(
-                height: 20,
-                width: 300,
-                child: Divider(
-                  color: Colors.deepPurple.shade100,
+      home: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: Scaffold(
+          backgroundColor: Colors.deepPurple,
+          body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/karl.jpg'),
                 ),
-              ),
-              Text(
-                'FLUTTER DEVELOPER',
-                style: TextStyle(
-                    fontFamily: 'SourceSansPro',
-                    color: Colors.teal.shade100,
-                    fontSize: 20.0,
-                    letterSpacing: 2.5,
-                    fontWeight: FontWeight.bold),
-              ),
-              Card(
-                color: Colors.white,
-                margin: const EdgeInsets.symmetric(
-                    vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: const Icon(Icons.phone, color: Colors.deepPurple),
-                  title: Text(
-                    '+1 999 999 9999',
-                    style: TextStyle(color: Colors.deepPurple.shade900),
+                const Text('Karl Urban',
+                    style: TextStyle(
+                        fontFamily: 'Pacifico',
+                        fontSize: 40.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold)),
+                SizedBox(
+                  height: 20,
+                  width: 300,
+                  child: Divider(
+                    color: Colors.deepPurple.shade100,
                   ),
                 ),
-              ),
-              Card(
+                Text(
+                  'FLUTTER DEVELOPER',
+                  style: TextStyle(
+                      fontFamily: 'SourceSansPro',
+                      color: Colors.teal.shade100,
+                      fontSize: 20.0,
+                      letterSpacing: 2.5,
+                      fontWeight: FontWeight.bold),
+                ),
+                Card(
                   color: Colors.white,
                   margin: const EdgeInsets.symmetric(
                       vertical: 10.0, horizontal: 25.0),
                   child: ListTile(
-                      leading: const Icon(
-                        Icons.email,
-                        color: Colors.deepPurple,
-                      ),
-                      title: Text(
-                        'karlurbarn@example.com',
-                        style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.deepPurple.shade900,
-                            fontFamily: 'SourceSansPro'),
-                      ))
-                  )
-            ],
+                    leading: const Icon(Icons.phone, color: Colors.deepPurple),
+                    title: Text(
+                      '+1 999 999 9999',
+                      style: TextStyle(color: Colors.deepPurple.shade900),
+                    ),
+                  ),
+                ),
+                Card(
+                    color: Colors.white,
+                    margin: const EdgeInsets.symmetric(
+                        vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                        leading: const Icon(
+                          Icons.email,
+                          color: Colors.deepPurple,
+                        ),
+                        title: Text(
+                          'karlurbarn@example.com',
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.deepPurple.shade900,
+                              fontFamily: 'SourceSansPro'),
+                        )))
+              ],
+            ),
           ),
         ),
       ),
