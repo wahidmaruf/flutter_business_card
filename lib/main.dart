7786e3c867fc613c7f52a6b4c1826d0b2d532202
Wahid Maruf <wahid.maruf@ibm.com>
2022-06-04 20:12:41 -0300

    Add image avatar, name, phone and email label

--- lib/main.dart
+++ lib/main.dart
@@ -9,20 +9,70 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.deepPurple,
-        body: Center(
-          child: Text(
-            "Hello World",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 25
-            ),
+        body: SafeArea(
+          child: Column(
+            children: [
+              const CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/karl.jpg'),
+              ),
+              const Text('Karl Urban',
+                  style: TextStyle(
+                      fontFamily: 'Pacifico',
+                      fontSize: 40.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold)),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    color: Colors.teal.shade100,
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              ),
+              Container(
+                color: Colors.white,
+                padding: const EdgeInsets.all(10),
+                margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: [
+                    const Icon(Icons.phone, color: Colors.deepPurple),
+                    const SizedBox(width: 10.0),
+                    Text('+1 999 999 9999',
+                      style: TextStyle(
+                        color: Colors.deepPurple.shade900
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: const EdgeInsets.all(10.0),
+                margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: [
+                    const Icon(Icons.email, color: Colors.deepPurple,),
+                    const SizedBox(width: 10.0),
+                    Text(
+                      'karlurbarn@example.com',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.deepPurple.shade900,
+                        fontFamily: 'SourceSansPro'
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
           ),
         ),
       ),
     );
   }
 }
-
